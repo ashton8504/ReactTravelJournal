b97501ce70a31b9882d92c662583706c609cb772
ashton8504 <ashtonlittle8504@gmail.com>
2023-06-14 10:07:38 -0400

    updated

--- Components/MainSection.js
+++ Components/MainSection.js
@@ -5,16 +5,16 @@ export default function MainSection({ title, location, googleMapsUrl, startDate,
     <div className="main-section">
       <div className="image-container">
         <img className="image" src={imageUrl} alt={title} />
-        <div className="content-container">
-          <div className="location-container">
-            <img className="location-image" src="./images/location.svg" alt="Location" />
-            <h3><span className="location">{location}</span> <span className="maps-link"> View on Google Maps</span></h3>
-          </div>
-          <h2>{title}</h2>
-          <p className="date--style">{startDate} - {endDate}</p>
-          <p>{description}</p>
-          <hr />
+      </div>
+      <div className="content-container">
+        <div className="location-container">
+          <span className="location">{location}</span>
+          <a className="maps-link" href={googleMapsUrl}>View on Google Maps</a>
         </div>
+        <h2>{title}</h2>
+        <p className="date--style">{startDate} - {endDate}</p>
+        <p>{description}</p>
+        <hr />
       </div>
     </div>
   );
